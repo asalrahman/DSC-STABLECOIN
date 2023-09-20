bfe1ce54fe2ddd7fcecdd89f9f777943d52980c7
asal rahman <asaln4825@gmail.com>
2023-09-20 14:26:28 +0530

    DSc engine defined

---
+++ contracts/DscEngine.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+/*
+-1 Token == $1 peg
+-Exogenous collateral
+-Algorimitically stable
+
+based on MakerDAO(DAI)
+
+*/
+contract DscEngine {
+
+    constructor() {}
+  
+  function depositeCollateralAndMintDsc() external {}
+
+  function redeemCollateralForDsc()external {}
+
+  function burnDsc() external{}
+
+  function Liquidation() external {}
+
+  function getHealfactor() external {}
+
+
+}
