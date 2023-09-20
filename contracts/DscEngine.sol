7632543760914db8d4db8e1b303f9c933cd77e53
asal rahman <asaln4825@gmail.com>
2023-09-21 01:10:24 +0530

    added deposite

--- contracts/DscEngine.sol
+++ contracts/DscEngine.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import {Dsc} from "./Dsc.sol";
+
+
 
 /*
 -1 Token == $1 peg
@@ -12,9 +15,42 @@ based on MakerDAO(DAI)
 */
 contract DscEngine {
 
-    constructor() {}
+   //errors
+   error DscEngine_NeedsMorethanZero();
+    error  DscEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+
+   //modifiers
+   modifier moreThanZero(uint256 amount) {
+    if(amount == 0){
+        revert DscEngine_NeedsMorethanZero();}
+    _;
+   }
+
+//    modifier isAllowed(address token) {
+//     if(s_)
+//    }
+
+
+
+    // Dsc private immutable i_dsc;
+
+
+    constructor(address[] memory tokenAddresses,address[]memory priceFeedaddresses,address dscAddress) {
+        
+        if(tokenAddresses.length != priceFeedaddresses.length){
+            revert  DscEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+            }
+
+         
+    }
   
   function depositeCollateralAndMintDsc() external {}
+
+  function  deposite(address tokenCollateralAddress,uint256 amountCollateral )external moreThanZero(amountCollateral){
+    
+
+
+  }
 
   function redeemCollateralForDsc()external {}
 
